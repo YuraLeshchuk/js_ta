841f1b9bb067fdb0c08b1a318323efbd018f6e1b
Yurii Leshchuk <ylesh@softserveinc.com>
2024-03-19 13:05:43 +0200

    added hw_1

---
+++ hw_1.js
@@ -0,0 +1,71 @@
+//task2
+
+// console.log("Leshchuk");
+
+//task3
+
+// let city = "Lviv";
+// let postCode = 79000;
+
+// alert(city);
+// alert(postCode);
+
+// city = postCode;
+
+// alert(city);
+// alert(postCode);
+
+//task4
+
+// const user = {
+//     name: "John",
+//     age: 30,
+//     married: false,
+//     pets: null,
+//     additionalInfo: undefined
+// };
+
+//task5
+
+// let isAdult = confirm("Are you 18+ years old?");
+// console.log(isAdult);
+
+//task6
+
+// let name;
+// let surname;
+// let group;
+// let birthYear;
+
+// name = "Yura";
+// surname = "Leschuk";
+// group = "TA-Q1"
+// birthYear = 1997;
+
+// let isMarried = false;
+
+// console.log(typeof(birthYear));
+// console.log(typeof(isMarried));
+// console.log(typeof(name));
+// console.log(typeof(surname));
+// console.log(typeof(group));
+
+// let emptyVariable;
+// console.log(typeof(emptyVariable));
+
+// let nullVar = null;
+// console.log(typeof(nullVar));
+
+//task7
+
+// let userLogin = prompt("Login", "Yura77");
+// let userPassword = prompt("Password", "1111");
+// let userEmail = prompt("Email", "yura@gmail.com");
+
+// alert(`Dear ${userLogin}, your password ${userPassword} for email address ${userEmail} is expired`);
+
+//task8
+
+// let hours = prompt("Show seconds in hours", 1);
+// let secInHour = hours * 60;
+// alert(`${hours} hour/s contains: ${secInHour} ${"seconds"}`);
